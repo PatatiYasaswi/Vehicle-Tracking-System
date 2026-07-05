f0dd89481c0f2b56e1300f6f22fcfbc4b7b199a1
Patati Yasaswi <patatiyasaswi316@gmail.com>
2026-07-05 22:05:52 +0530

    Add Railway database port support

--- database.py
+++ database.py
@@ -11,10 +11,11 @@ def get_database_connection():
     """
     try:
         connection = mysql.connector.connect(
-            host=Config.DB_HOST,
-            user=Config.DB_USER,
-            password=Config.DB_PASSWORD,
-            database=Config.DB_NAME
+        host=Config.DB_HOST,
+        port=Config.DB_PORT,
+        user=Config.DB_USER,
+        password=Config.DB_PASSWORD,
+        database=Config.DB_NAME
         )
         return connection
     except mysql.connector.Error as err:
